6645f8870fffe2c7887e5c0b54fbcd1445c8e674
Antonio Moura (@antl3x) <aasm.moura@gmail.com>
2024-05-07 11:31:16 -0600

    #0

--- minirepos/@totuna-cli/src/commands/migrations/run.ts
+++ minirepos/@totuna-cli/src/commands/migrations/run.ts
@@ -1,4 +1,4 @@
-import * as migrations from '@totuna/core/migrations/@api'
+import * as migrations from 'migrations/@api.js'
 import fs from 'fs'
 import path from 'path'
 import {BaseCommand} from 'commands/BaseCommand.js'
@@ -25,42 +25,56 @@ export default class Command extends BaseCommand<typeof Command> {
   /* ----------------------------------- run ---------------------------------- */
 
   public async run(): Promise<void> {
-    logger.debug('Running migrations.')
     const spinner = ora().start()
 
-    if (this.args.plan) {
-      spinner.text = 'Fetching migrations from plan folder..'
+    try {
+      logger.debug('Running migrations.')
 
-      // Get last migration Id to rename plan files
-      const nextMigID = await migrations.getNextMigrationSeq('local')
-      // Rename plan files
-      const planPath = this.rootStore.systemVariables.PUBLIC_MIGRATIONS_PLAN_PATH
-      const migrationsPath = this.rootStore.systemVariables.PUBLIC_MIGRATIONS_PATH
+      if (this.args.plan) {
+        spinner.text = 'Fetching migrations from plan folder..'
 
-      // Get plan files and their stats
-      const planFiles = fs.readdirSync(planPath).map((file) => {
-        return {
-          name: file,
-          time: fs.statSync(path.join(planPath, file)).birthtime,
+        // Get last migration Id to rename plan files
+        const nextMigID = await migrations.getNextMigrationSeq('local')
+        // Rename plan files
+        const planPath = this.rootStore.systemVariables.PUBLIC_MIGRATIONS_PLAN_PATH
+        const migrationsPath = this.rootStore.systemVariables.PUBLIC_MIGRATIONS_PATH
+
+        // Get plan files and their stats
+        const planFiles = fs.readdirSync(planPath).map((file) => {
+          return {
+            name: file,
+            time: fs.statSync(path.join(planPath, file)).birthtime,
+          }
+        })
+
+        // Sort files by creation time
+        planFiles.sort((a, b) => a.time.getTime() - b.time.getTime())
+
+        for (const planFile of planFiles) {
+          const planFilePath = path.join(planPath, planFile.name)
+          const newPlanFile = `${nextMigID}_${new Date().getTime()}_${planFile.name}`
+          const newPlanFilePath = path.join(migrationsPath, newPlanFile)
+          fs.renameSync(planFilePath, newPlanFilePath)
         }
-      })
-
-      // Sort files by creation time
-      planFiles.sort((a, b) => a.time.getTime() - b.time.getTime())
-
-      for (const planFile of planFiles) {
-        const planFilePath = path.join(planPath, planFile.name)
-        const newPlanFile = `${nextMigID}_${new Date().getTime()}_${planFile.name}`
-        const newPlanFilePath = path.join(migrationsPath, newPlanFile)
-        fs.renameSync(planFilePath, newPlanFilePath)
       }
+
+      // Run migrations
+      spinner.text = 'Executing migrations..'
+
+      const res = await migrations.migrate()
+      if (res.length > 0) {
+        res.forEach((migration) => {
+          this.log(`\x1b[32m✔ Migration file executed: ${migration.name}\x1b[0m`)
+        })
+        spinner.succeed('Migrations executed successfully!')
+      } else {
+        spinner.info('No migrations to execute.')
+      }
+
+      return res
+    } catch (error) {
+      spinner.fail('Failed to execute migrations.')
+      throw new Error(`Failed to execute migrations: ${error}`, {cause: error})
     }
-
-    // Run migrations
-    spinner.text = 'Executing migrations..'
-
-    const res = await migrations.migrate()
-
-    spinner.succeed('Migrations executed successfully!')
   }
 }
